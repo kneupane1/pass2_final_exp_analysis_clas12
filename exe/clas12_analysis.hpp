c1f3c2674a2ef5fbcc4a8e2064a83a98ae4784e1
Krishna Neupane <35571116+kneupane1@users.noreply.github.com>
2025-03-11 10:56:16 -0400

    Update clas12_analysis.hpp

--- exe/clas12_analysis.hpp
+++ exe/clas12_analysis.hpp
@@ -408,11 +408,18 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram> &_hi
                                                                                         dp_Prot = (trackFD.P() - trackCD.P());
                                                                                         dtheta_Prot = (trackFD.Theta() - trackCD.Theta()) * 180 / PI;
                                                                                         dphi_Prot = (trackFD.Phi() - trackCD.Phi()) * 180 / PI;
-                                                                                        _hists->Fill_cdfd_prot(dp_Prot, dtheta_Prot, dphi_Prot, event);
-
+                                                                                        // _hists->Fill_cdfd_prot(dp_Prot, dtheta_Prot, dphi_Prot, event);
                                                                                         // Apply proton cuts
-                                                                                        // if (dp_Prot > -0.6 && dp_Prot < 0.2 && dtheta_Prot > -7 && dphi_Prot > -20 && dphi_Prot < 5)
-                                                                                        if (dp_Prot > -0.3 && dp_Prot < 0.0 && dtheta_Prot > -3 && dtheta_Prot < 3 && dphi_Prot > -5 && dphi_Prot < 2)
+                                                                                        /// first try // if (dp_Prot > -0.6 && dp_Prot < 0.2 && dtheta_Prot > -7 && dphi_Prot > -20 && dphi_Prot < 5)
+                                                                                        ////// mostly used  if (dp_Prot > -0.3 && dp_Prot < 0.0 && dtheta_Prot > -3 && dtheta_Prot < 3 && dphi_Prot > -5 && dphi_Prot < 2)
+
+                                                                                        /////////new //
+                                                                                        // if (dp_Prot > -0.25 && dp_Prot < 0.02 && dtheta_Prot > -3 && dtheta_Prot < 3 && dphi_Prot > -10 && dphi_Prot < 5)
+                                                                                        /////tight
+                                                                                        if (dp_Prot > -0.3 && dp_Prot < 0.07 && dtheta_Prot > -4 && dtheta_Prot < 4 && dphi_Prot > -12.5 && dphi_Prot < 7.5)
+                                                                                        ////// loose
+                                                                                        //         if (dp_Prot > -0.2 && dp_Prot < -0.02 && dtheta_Prot > -2 && dtheta_Prot < 2 && dphi_Prot > -7.5 && dphi_Prot < 2.5)
+
                                                                                         {
                                                                                                 proton_cdfd_cut = true;
                                                                                                 //  // } else {  // Fill histograms
@@ -456,15 +463,20 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram> &_hi
                                                                                         dp_Pip = (trackFD.P() - trackCD.P());
                                                                                         dtheta_Pip = (trackFD.Theta() - trackCD.Theta()) * 180 / PI;
                                                                                         dphi_Pip = (trackFD.Phi() - trackCD.Phi()) * 180 / PI;
-                                                                                        _hists->Fill_cdfd_pip(dp_Pip, dtheta_Pip, dphi_Pip, event);
+                                                                                        // _hists->Fill_cdfd_pip(dp_Pip, dtheta_Pip, dphi_Pip, event);
 
-                                                                                        // h_dp_pip->Fill(dp_Pip, event->weight());
-                                                                                        // h_dtheta_pip->Fill(dtheta_Pip, event->weight());
-                                                                                        // h_dphi_pip->Fill(dphi_Pip, event->weight());
                                                                                         // Apply pip cuts
-                                                                                        // if (dp_Pip > -0.4 && dp_Pip < 0.2 && dtheta_Pip > -10 && dtheta_Pip < 10 && dphi_Pip > -20 &&
+                                                                                        /// initially used // if (dp_Pip > -0.4 && dp_Pip < 0.2 && dtheta_Pip > -10 && dtheta_Pip < 10 && dphi_Pip > -20 &&
                                                                                         //     dphi_Pip < 5)
-                                                                                        if (dp_Pip > -0.25 && dp_Pip < 0.0 && dtheta_Pip > -3 && dtheta_Pip < 3 && dphi_Pip > -5 && dphi_Pip < 2)
+                                                                                        ////// mostly used until march 11
+                                                                                        // if (dp_Pip > -0.25 && dp_Pip < 0.0 && dtheta_Pip > -3 && dtheta_Pip < 3 && dphi_Pip > -5 && dphi_Pip < 2)
+                                                                                        /////// NEW MID
+                                                                                        // if (dp_Pip > -0.15 && dp_Pip < 0.05 && dtheta_Pip > -3 && dtheta_Pip < 3 && dphi_Pip > -10 && dphi_Pip < 5)
+
+                                                                                        ///// NEW TIGHT
+                                                                                        if (dp_Pip > -0.2 && dp_Pip < 0.1 && dtheta_Pip > -4 && dtheta_Pip < 4 && dphi_Pip > -12.5 && dphi_Pip < 7.5)
+                                                                                        /////// NEW LOOSE
+                                                                                        // if (dp_Pip > -0.1 && dp_Pip < 0.0 && dtheta_Pip > -2 && dtheta_Pip < 2 && dphi_Pip > -7.5 && dphi_Pip < 2.5)
                                                                                         {
                                                                                                 pip_cdfd_cut = true;
                                                                                                 // // } else {  // Fill histograms
@@ -483,7 +495,7 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram> &_hi
                                         {
                                                 for (size_t j = 0; j < num_pips; ++j)
                                                 {
-                                                        if (!(proton_cdfd_cut == true || pip_cdfd_cut == true))
+                                                        // if (!(proton_cdfd_cut == true || pip_cdfd_cut == true))
                                                         //////                                                        if ((proton_cdfd_cut == true || pip_cdfd_cut == true))
                                                         {
                                                                 // Exclude the case where the same particle is assigned as both proton and pip
@@ -494,6 +506,9 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram> &_hi
 
                                                                         // if ((best_proton_index != event->GetProtonIndices()[i]) || (best_pip_index != event->GetPipIndices()[j]))
                                                                         {
+
+                                                                                _hists->Fill_cdfd_pip(dp_Pip, dtheta_Pip, dphi_Pip, event);
+                                                                                _hists->Fill_cdfd_prot(dp_Prot, dtheta_Prot, dphi_Prot, event);
 
                                                                                 two_pion_mPim_events++;
                                                                                 entries_in_this_event++;
